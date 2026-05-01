0bd1bebf5ac593cdfd5b519e51f5b85fd8991a49
Iram-Bhuiyan <itbhuiyan@gmail.com>
2026-05-01 17:49:36 +0600

    Add basic calculator function

--- srcmain.py
+++ srcmain.py
@@ -2,3 +2,13 @@ import datetime
 
 print("Name: Iram")
 print("Date:", datetime.date.today())
+
+import datetime
+from utils import add, subtract
+
+print("Name: Iram")
+print("Date:", datetime.date.today())
+
+# Calculator functions
+print("Add:", add(5, 3))
+print("Subtract:", subtract(10, 4))
